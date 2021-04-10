b5280c009315bfc785fc4ff3d5a4815a410e5c9a
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-04-10 04:27:40 -0400

    Fix intervention image import on AdminController

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
-use Intervention\Image\Image;
+use Image;
 use function PHPUnit\Framework\isReadable;
 
 
