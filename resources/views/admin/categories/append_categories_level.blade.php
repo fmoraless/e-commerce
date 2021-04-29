0481b0d0d9e4a415038017fa89842b5aaa6676e0
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-04-29 17:58:16 -0400

    Append categories for levels parent id

--- resources/views/admin/categories/append_categories_level.blade.php
+++ resources/views/admin/categories/append_categories_level.blade.php
@@ -1,10 +1,17 @@
 <div class="form-group">
     <label>Nivel Categoría</label>
     <select id="parent_id" name="parent_id" class="form-control select2" style="width: 100%;">
-        <option value="0">Categoría principal</option>
+        <option value="0" @if(isset($categorydata['parent_id']) && $categorydata['parent_id']==0)
+            selected="" @endif>
+            Categoría principal
+        </option>
         @if(!empty($categories))
             @foreach($categories as $category)
-                <option value="{{ $category['id'] }}">{{ $category['category_name'] }}</option>
+                <option value="{{ $category['id'] }}" @if(isset($categorydata['parent_id']) &&
+                            $categorydata['parent_id']==$category['id'])
+                selected="" @endif>
+                    {{ $category['category_name'] }}
+                </option>
                 @if(!empty($category['subcategories']))
                     @foreach($category['subcategories'] as $subcategory)
                         <option value="{{ $subcategory['id'] }}">&nbsp;&raquo;&nbsp;{{ $subcategory['category_name'] }}</option>
