735584bf4fe3f4deeed75854f37a0ef07d952c98
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-04-29 21:44:14 -0400

    route to delete category

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,7 @@ Route::prefix('/admin')->namespace('Admin')->group(function (){
         Route::post('append-categories-level', 'CategoryController@appendCategoryLevel');
 
         Route::get('delete-category-image/{id}', 'CategoryController@deleteCategoryImage');
+        Route::get('delete-category/{id}', 'CategoryController@deleteCategory');
     });
 
 });
